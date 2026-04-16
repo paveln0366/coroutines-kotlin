b71b9ec50b7d96474debbb721ccd3dafd44404ed
Pavel Potapov <potapov@goozix.com>
2026-04-16 17:49:32 +0300

    Add callbacks

--- src/main/kotlin/callbacks/Display.kt
+++ src/main/kotlin/callbacks/Display.kt
@@ -5,6 +5,7 @@ import org.example.entities.Book
 import java.awt.BorderLayout
 import java.awt.Dimension
 import javax.swing.*
+import kotlin.concurrent.thread
 
 object Display {
 
@@ -16,12 +17,14 @@ object Display {
         addActionListener {
             isEnabled = false
             infoArea.text = "Loading book information...\n"
-            val book = loadBook()
-            infoArea.append("Book: ${book.title}\nYear: ${book.year}\nGenre: ${book.genre}\n")
-            infoArea.append("Load author information...\n")
-            val author = loadAuthor(book)
-            infoArea.append("Author: ${author.name}\nBiography: ${author.bio}\n")
-            isEnabled = true
+            loadBook { book ->
+                infoArea.append("Book: ${book.title}\nYear: ${book.year}\nGenre: ${book.genre}\n")
+                infoArea.append("Load author information...\n")
+                loadAuthor(book) { author ->
+                    infoArea.append("Author: ${author.name}\nBiography: ${author.bio}\n")
+                    isEnabled = true
+                }
+            }
         }
     }
     private val timerLabel = JLabel("Timer: 00:00")
@@ -42,24 +45,30 @@ object Display {
         startTimer()
     }
 
-    private fun loadBook(): Book {
-        Thread.sleep(3000)
-        return Book("1984", 1949, "Dystopia")
+    private fun loadBook(callback: (Book) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callback(Book("1984", 1949, "Dystopia"))
+        }
     }
 
-    private fun loadAuthor(book: Book): Author {
-        Thread.sleep(3000)
-        return Author("George Orwell", "British writer and journalist")
+    private fun loadAuthor(book: Book, callback: (Author) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callback(Author("George Orwell", "British writer and journalist"))
+        }
     }
 
     private fun startTimer() {
-        var totalSeconds = 0
-        while (true) {
-            val minutes = totalSeconds / 60
-            val seconds = totalSeconds % 60
-            timerLabel.text = String.format("Time: %02d:%02d", minutes, seconds)
-            Thread.sleep(1000)
-            totalSeconds++
+        thread {
+            var totalSeconds = 0
+            while (true) {
+                val minutes = totalSeconds / 60
+                val seconds = totalSeconds % 60
+                timerLabel.text = String.format("Time: %02d:%02d", minutes, seconds)
+                Thread.sleep(1000)
+                totalSeconds++
+            }
         }
     }
 }
