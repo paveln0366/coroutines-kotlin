9e62769ef370321471f1911bd3baec9dd7890fa4
Pavel Potapov <potapov@goozix.com>
2026-04-28 17:07:56 +0300

    Add exception

---
+++ src/main/kotlin/exception/Test.kt
@@ -0,0 +1,29 @@
+package org.example.exception
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+
+private val exceptionHandler = CoroutineExceptionHandler { _, _ ->
+    println("Exception caught")
+}
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val scope = CoroutineScope(dispatcher + CoroutineName("My coroutine") + Job() + exceptionHandler)
+
+fun main() {
+    scope.launch {
+        method()
+    }
+    scope.launch {
+        method2()
+    }
+}
+
+suspend fun method(): String {
+    delay(3000)
+    error("")
+}
+
+suspend fun method2() {
+    delay(5000)
+    println("Method2 is finished")
+}
