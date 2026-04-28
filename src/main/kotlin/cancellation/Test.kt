f420236ba559e991dcb57c3c8ae44e4648be2d86
Pavel Potapov <potapov@goozix.com>
2026-04-28 17:56:26 +0300

    Add cancellation

---
+++ src/main/kotlin/cancellation/Test.kt
@@ -0,0 +1,32 @@
+package org.example.cancellation
+
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
+
+private val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
+private val scope = CoroutineScope(dispatcher)
+
+fun main() {
+    val job = scope.launch {
+        timer()
+    }
+    Thread.sleep(3000)
+    job.cancel()
+}
+
+private suspend fun timer() {
+    withContext(dispatcher) {
+        var seconds = 0
+        while (true) {
+            try {
+                ensureActive()
+                println(seconds++)
+                Thread.sleep(1000)
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+}
